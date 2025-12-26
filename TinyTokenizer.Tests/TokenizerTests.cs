577a38f4475c2bcae7975cb74767c9ea5101c647
David Sisco <dsisco11@gmail.com>
2025-12-25 16:50:00 -0800

    test coverage for comment tokens

--- TinyTokenizer.Tests/TokenizerTests.cs
+++ TinyTokenizer.Tests/TokenizerTests.cs
@@ -342,6 +342,340 @@ public class TokenizerTests
 
     #endregion
 
+    #region Numeric Token Tests
+
+    [Fact]
+    public void Tokenize_IntegerLiteral_ReturnsNumericToken()
+    {
+        var tokens = Tokenize("123");
+
+        Assert.Single(tokens);
+        var numeric = Assert.IsType<NumericToken>(tokens[0]);
+        Assert.Equal("123", numeric.ContentSpan.ToString());
+        Assert.Equal(NumericType.Integer, numeric.NumericType);
+    }
+
+    [Fact]
+    public void Tokenize_FloatingPointLiteral_ReturnsNumericToken()
+    {
+        var tokens = Tokenize("3.14");
+
+        Assert.Single(tokens);
+        var numeric = Assert.IsType<NumericToken>(tokens[0]);
+        Assert.Equal("3.14", numeric.ContentSpan.ToString());
+        Assert.Equal(NumericType.FloatingPoint, numeric.NumericType);
+    }
+
+    [Fact]
+    public void Tokenize_FloatingPointStartingWithDot_ReturnsNumericToken()
+    {
+        var tokens = Tokenize(".5");
+
+        Assert.Single(tokens);
+        var numeric = Assert.IsType<NumericToken>(tokens[0]);
+        Assert.Equal(".5", numeric.ContentSpan.ToString());
+        Assert.Equal(NumericType.FloatingPoint, numeric.NumericType);
+    }
+
+    [Fact]
+    public void Tokenize_IntegerFollowedByDotWithoutDigit_ReturnsIntegerAndSymbol()
+    {
+        var tokens = Tokenize("123.");
+
+        Assert.Equal(2, tokens.Length);
+        var numeric = Assert.IsType<NumericToken>(tokens[0]);
+        Assert.Equal("123", numeric.ContentSpan.ToString());
+        Assert.Equal(NumericType.Integer, numeric.NumericType);
+        Assert.IsType<SymbolToken>(tokens[1]);
+    }
+
+    [Fact]
+    public void Tokenize_MultipleNumericLiterals_ReturnsMultipleTokens()
+    {
+        var tokens = Tokenize("1 2.5 3");
+
+        Assert.Equal(5, tokens.Length);
+        var num1 = Assert.IsType<NumericToken>(tokens[0]);
+        Assert.Equal(NumericType.Integer, num1.NumericType);
+        Assert.IsType<WhitespaceToken>(tokens[1]);
+        var num2 = Assert.IsType<NumericToken>(tokens[2]);
+        Assert.Equal(NumericType.FloatingPoint, num2.NumericType);
+        Assert.IsType<WhitespaceToken>(tokens[3]);
+        var num3 = Assert.IsType<NumericToken>(tokens[4]);
+        Assert.Equal(NumericType.Integer, num3.NumericType);
+    }
+
+    #endregion
+
+    #region String Token Tests
+
+    [Fact]
+    public void Tokenize_DoubleQuotedString_ReturnsStringToken()
+    {
+        var tokens = Tokenize("\"hello\"");
+
+        Assert.Single(tokens);
+        var str = Assert.IsType<StringToken>(tokens[0]);
+        Assert.Equal("\"hello\"", str.ContentSpan.ToString());
+        Assert.Equal('"', str.Quote);
+        Assert.Equal("hello", str.Value.ToString());
+    }
+
+    [Fact]
+    public void Tokenize_SingleQuotedString_ReturnsStringToken()
+    {
+        var tokens = Tokenize("'hello'");
+
+        Assert.Single(tokens);
+        var str = Assert.IsType<StringToken>(tokens[0]);
+        Assert.Equal("'hello'", str.ContentSpan.ToString());
+        Assert.Equal('\'', str.Quote);
+        Assert.Equal("hello", str.Value.ToString());
+    }
+
+    [Fact]
+    public void Tokenize_EmptyString_ReturnsStringTokenWithEmptyValue()
+    {
+        var tokens = Tokenize("\"\"");
+
+        Assert.Single(tokens);
+        var str = Assert.IsType<StringToken>(tokens[0]);
+        Assert.Equal("\"\"", str.ContentSpan.ToString());
+        Assert.True(str.Value.IsEmpty);
+    }
+
+    [Fact]
+    public void Tokenize_StringWithEscapedQuote_IncludesEscapeSequence()
+    {
+        var tokens = Tokenize("\"say \\\"hi\\\"\"");
+
+        Assert.Single(tokens);
+        var str = Assert.IsType<StringToken>(tokens[0]);
+        Assert.Equal("\"say \\\"hi\\\"\"", str.ContentSpan.ToString());
+    }
+
+    [Fact]
+    public void Tokenize_StringWithEscapedBackslash_IncludesEscapeSequence()
+    {
+        var tokens = Tokenize("\"path\\\\file\"");
+
+        Assert.Single(tokens);
+        var str = Assert.IsType<StringToken>(tokens[0]);
+        Assert.Equal("\"path\\\\file\"", str.ContentSpan.ToString());
+    }
+
+    [Fact]
+    public void Tokenize_UnterminatedString_ReturnsSymbolAndContinues()
+    {
+        var tokens = Tokenize("\"hello");
+
+        // Should emit the quote as a symbol, then continue with "hello" as text
+        Assert.Equal(2, tokens.Length);
+        var symbol = Assert.IsType<SymbolToken>(tokens[0]);
+        Assert.Equal('"', symbol.Symbol);
+        var text = Assert.IsType<TextToken>(tokens[1]);
+        Assert.Equal("hello", text.ContentSpan.ToString());
+    }
+
+    [Fact]
+    public void Tokenize_UnterminatedSingleQuotedString_ReturnsSymbolAndContinues()
+    {
+        var tokens = Tokenize("'hello");
+
+        Assert.Equal(2, tokens.Length);
+        var symbol = Assert.IsType<SymbolToken>(tokens[0]);
+        Assert.Equal('\'', symbol.Symbol);
+        var text = Assert.IsType<TextToken>(tokens[1]);
+        Assert.Equal("hello", text.ContentSpan.ToString());
+    }
+
+    [Fact]
+    public void Tokenize_MixedStringAndText_ReturnsCorrectTokens()
+    {
+        var tokens = Tokenize("name=\"value\"");
+
+        Assert.Equal(3, tokens.Length);
+        Assert.IsType<TextToken>(tokens[0]);
+        Assert.IsType<SymbolToken>(tokens[1]);
+        var str = Assert.IsType<StringToken>(tokens[2]);
+        Assert.Equal("value", str.Value.ToString());
+    }
+
+    [Fact]
+    public void Tokenize_StringWithSpaces_PreservesSpaces()
+    {
+        var tokens = Tokenize("\"hello world\"");
+
+        Assert.Single(tokens);
+        var str = Assert.IsType<StringToken>(tokens[0]);
+        Assert.Equal("hello world", str.Value.ToString());
+    }
+
+    [Fact]
+    public void Tokenize_StringWithNumbers_PreservesNumbers()
+    {
+        var tokens = Tokenize("\"value is 42\"");
+
+        Assert.Single(tokens);
+        var str = Assert.IsType<StringToken>(tokens[0]);
+        Assert.Equal("value is 42", str.Value.ToString());
+    }
+
+    #endregion
+
+    #region Comment Token Tests
+
+    [Fact]
+    public void Tokenize_SingleLineComment_ReturnsCommentToken()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(CommentStyle.CStyleSingleLine);
+        var tokens = Tokenize("// this is a comment", options);
+
+        Assert.Single(tokens);
+        var comment = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.Equal("// this is a comment", comment.ContentSpan.ToString());
+        Assert.False(comment.IsMultiLine);
+    }
+
+    [Fact]
+    public void Tokenize_SingleLineComment_StopsAtNewline()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(CommentStyle.CStyleSingleLine);
+        var tokens = Tokenize("// comment\ncode", options);
+
+        Assert.Equal(3, tokens.Length);
+        var comment = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.Equal("// comment", comment.ContentSpan.ToString());
+        Assert.IsType<WhitespaceToken>(tokens[1]);
+        Assert.IsType<TextToken>(tokens[2]);
+    }
+
+    [Fact]
+    public void Tokenize_MultiLineComment_ReturnsCommentToken()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(CommentStyle.CStyleMultiLine);
+        var tokens = Tokenize("/* this is a comment */", options);
+
+        Assert.Single(tokens);
+        var comment = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.Equal("/* this is a comment */", comment.ContentSpan.ToString());
+        Assert.True(comment.IsMultiLine);
+    }
+
+    [Fact]
+    public void Tokenize_MultiLineComment_SpansMultipleLines()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(CommentStyle.CStyleMultiLine);
+        var tokens = Tokenize("/* line 1\nline 2 */", options);
+
+        Assert.Single(tokens);
+        var comment = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.Equal("/* line 1\nline 2 */", comment.ContentSpan.ToString());
+        Assert.True(comment.IsMultiLine);
+    }
+
+    [Fact]
+    public void Tokenize_UnterminatedMultiLineComment_ConsumesToEnd()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(CommentStyle.CStyleMultiLine);
+        var tokens = Tokenize("/* unclosed comment", options);
+
+        Assert.Single(tokens);
+        var comment = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.Equal("/* unclosed comment", comment.ContentSpan.ToString());
+        Assert.True(comment.IsMultiLine);
+    }
+
+    [Fact]
+    public void Tokenize_HashComment_ReturnsCommentToken()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(CommentStyle.HashSingleLine);
+        var tokens = Tokenize("# python comment", options);
+
+        Assert.Single(tokens);
+        var comment = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.Equal("# python comment", comment.ContentSpan.ToString());
+        Assert.False(comment.IsMultiLine);
+    }
+
+    [Fact]
+    public void Tokenize_SqlComment_ReturnsCommentToken()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(CommentStyle.SqlSingleLine);
+        var tokens = Tokenize("-- sql comment", options);
+
+        Assert.Single(tokens);
+        var comment = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.Equal("-- sql comment", comment.ContentSpan.ToString());
+        Assert.False(comment.IsMultiLine);
+    }
+
+    [Fact]
+    public void Tokenize_HtmlComment_ReturnsCommentToken()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(CommentStyle.HtmlComment);
+        var tokens = Tokenize("<!-- html comment -->", options);
+
+        Assert.Single(tokens);
+        var comment = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.Equal("<!-- html comment -->", comment.ContentSpan.ToString());
+        Assert.True(comment.IsMultiLine);
+    }
+
+    [Fact]
+    public void Tokenize_MultipleCommentStyles_RecognizesBoth()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(
+            CommentStyle.CStyleSingleLine, 
+            CommentStyle.CStyleMultiLine);
+        var tokens = Tokenize("// single\n/* multi */", options);
+
+        Assert.Equal(3, tokens.Length);
+        var single = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.False(single.IsMultiLine);
+        Assert.IsType<WhitespaceToken>(tokens[1]);
+        var multi = Assert.IsType<CommentToken>(tokens[2]);
+        Assert.True(multi.IsMultiLine);
+    }
+
+    [Fact]
+    public void Tokenize_CommentBeforeCode_ReturnsCorrectOrder()
+    {
+        var options = TokenizerOptions.Default.WithCommentStyles(CommentStyle.CStyleSingleLine);
+        var tokens = Tokenize("// comment\nfunc()", options);
+
+        Assert.Equal(4, tokens.Length);
+        Assert.IsType<CommentToken>(tokens[0]);
+        Assert.IsType<WhitespaceToken>(tokens[1]);
+        Assert.IsType<TextToken>(tokens[2]);
+        Assert.IsType<BlockToken>(tokens[3]);
+    }
+
+    [Fact]
+    public void Tokenize_NoCommentStylesConfigured_TreatsAsSymbols()
+    {
+        // Default options have no comment styles
+        var tokens = Tokenize("// not a comment");
+
+        // Should be parsed as symbols and text, not as a comment
+        Assert.True(tokens.Length > 1);
+        Assert.DoesNotContain(tokens, t => t is CommentToken);
+    }
+
+    [Fact]
+    public void Tokenize_CustomCommentStyle_IsRecognized()
+    {
+        var customStyle = new CommentStyle("REM ");
+        var options = TokenizerOptions.Default.WithCommentStyles(customStyle);
+        var tokens = Tokenize("REM this is a batch comment", options);
+
+        Assert.Single(tokens);
+        var comment = Assert.IsType<CommentToken>(tokens[0]);
+        Assert.Equal("REM this is a batch comment", comment.ContentSpan.ToString());
+    }
+
+    #endregion
+
     #region Utility Extension Tests
 
     [Fact]
