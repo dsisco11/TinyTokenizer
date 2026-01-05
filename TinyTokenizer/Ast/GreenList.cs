687f2f32c1606919755ab27f76f6ce815090ddcb
David Sisco <dsisco11@gmail.com>
2026-01-04 18:14:04 -0800

    chore: move RedList to its own file

--- TinyTokenizer/Ast/GreenList.cs
+++ TinyTokenizer/Ast/GreenList.cs
@@ -133,28 +133,3 @@ internal sealed record GreenList : GreenContainer
     
     #endregion
 }
-
-/// <summary>
-/// Red node wrapper for the root token list.
-/// </summary>
-[DebuggerDisplay("{DebuggerDisplay,nq}")]
-public sealed class RedList : RedNode
-{
-    /// <inheritdoc/>
-    protected override string DebuggerDisplay =>
-        $"List[{Position}..{EndPosition}] ({SlotCount} children) \"{Truncate(ToText(), 20)}\"";
-    
-    /// <summary>
-    /// Creates a new red list.
-    /// </summary>
-    internal RedList(GreenList green, RedNode? parent, int position, int siblingIndex = -1, SyntaxTree? tree = null)
-        : base(green, parent, position, siblingIndex, tree)
-    {
-    }
-    
-    /// <summary>The underlying green list.</summary>
-    internal new GreenList Green => (GreenList)base.Green;
-    
-    /// <summary>Number of children.</summary>
-    public int ChildCount => Green.SlotCount;
-}
